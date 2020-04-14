ce16b9393ebbf56ee0c0e1c145c3c18c2f7a64bf
ul18363 <49818957+ul18363@users.noreply.github.com>
2020-04-14 22:22:23 +0100

    Update disaster_recovery.sh

--- disaster_recovery.sh
+++ disaster_recovery.sh
@@ -1,3 +1,11 @@
+#Install Python
+sudo apt install python
 #Install GitKraken
+## Get Dependencies
+sudo apt-get install gconf2
+sudo apt-get install gconf-service
+## Get Files and install
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
+# Install Chromium
+sudo apt-get install chromium-browser
