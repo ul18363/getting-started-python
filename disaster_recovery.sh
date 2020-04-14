7826563d0d47b67ee300663a1a988f0032b2e63e
ul18363 <49818957+ul18363@users.noreply.github.com>
2020-04-14 22:27:23 +0100

    Update disaster_recovery.sh

--- disaster_recovery.sh
+++ disaster_recovery.sh
@@ -12,3 +12,6 @@ sudo dpkg -i gitkraken-amd64.deb
 sudo apt-get install chromium-browser
 # Install Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
