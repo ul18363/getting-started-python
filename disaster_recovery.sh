f17356ff311d8cbc679b53889c61de520ba46cad
ul18363 <49818957+ul18363@users.noreply.github.com>
2020-04-14 22:25:54 +0100

    Update disaster_recovery.sh

--- disaster_recovery.sh
+++ disaster_recovery.sh
@@ -7,5 +7,8 @@ sudo apt-get install gconf-service
 ## Get Files and install
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
+### Note: If something fails you can use 'sudo apt --fix-broken install' to get the dependencies
 # Install Chromium
 sudo apt-get install chromium-browser
+# Install Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
