e4480febde2c3ba1bd5ade11b179093bea2590ca
Bruno Castro <ul18363@bristol.ac.ul>
2020-04-15 01:15:08 +0100

    x

--- disaster_recovery/step1_disaster_recovery.sh
+++ disaster_recovery/step1_disaster_recovery.sh
@@ -1,2 +1,5 @@
 # Create Anaconda App shortuct
+cd /media/bruno/Extra/Ubuntu-Repository/Personal\ Repo/getting-started-python/disaster_recovery/
+
+sudo cp anaconda_icon.png /home/bruno/Pictures
 sudo cp Anaconda.desktop /usr/share/applications
