b0252beef0b9d1923052caa55894b77796d99e13
Bruno <ul18363@bristol.ac.uk>
2020-06-12 09:01:28 +0100

    Brughtness controller

--- disaster_recovery/step1_disaster_recovery.sh
+++ disaster_recovery/step1_disaster_recovery.sh
@@ -1,5 +1,8 @@
 # Create Anaconda App shortuct
 #cd /media/bruno/Extra/Ubuntu-Repository/Personal\ Repo/getting-started-python/disaster_recovery/
-
+# Create Anaconda Shortcut
 sudo cp anaconda.png /home/bruno/Pictures
 sudo cp Anaconda.desktop /usr/share/applications
+# Install screen brightness controller
+sudo add-apt-repository ppa:apandada1/brightness-controller
+sudo apt update && sudo apt install brightness-controller
